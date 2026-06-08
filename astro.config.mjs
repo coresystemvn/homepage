1b761a068b685297eccd9b1b3ae0ca41a064cb49
CoreSystem <12759295+coresystemvn@users.noreply.github.com>
2026-06-08 22:40:48 +0700

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import relativeLinks from 'astro-relative-links';
+
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://whydevils.github.io',
-  base: '/astro-scholar/',
-  integrations: [relativeLinks()],
+  site: 'https://coresystemvn.github.io',
+  base: '/homepage/',
+ 
 });
