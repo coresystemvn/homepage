381c9cc39d97ebe0a2586619b4fee7a46dfc6b5e
Trần Trung Trực <tructransecure@gmail.com>
2026-06-09 12:52:45 +0700

    Update

--- src/constants.ts
+++ src/constants.ts
@@ -36,6 +36,6 @@ export const SITE_DATA = {
 
     // Base URL of your deployed site (no trailing slash), e.g. "https://your-username.github.io"
     // Used for Open Graph tags. Leave empty if you haven't deployed yet.
-    // siteUrl: "https://coresystemvn.pages.dev",
-    siteUrl: "https://blog.ngantrambh.workers.dev",
+    //siteUrl: "https://blog.ngantrambh.workers.dev",
+    siteUrl: "https://coresystem.vn",
 };
